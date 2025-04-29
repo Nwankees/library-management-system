766715dc57885b090f711692e14f4ec428433c4a
Nwankees <eenwankwo12@gmail.com>
2025-04-28 22:44:39 -0400

    Fix: Rename Images to images for static files

--- library_site/settings.py
+++ library_site/settings.py
@@ -119,7 +119,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
